0659fc1fd8ff31257f4ba2e4c28247b4a447c98c
Daniel Wiesner <daniel_wiesner@gmx.de>
2022-05-14 13:04:26 +0200

    Rename files to match, adjust readme to reflect new bank

--- README.md
+++ README.md
@@ -1,54 +1,36 @@
-dkb2homebank ![Python application](https://github.com/hamvocke/dkb2homebank/workflows/Python%20application/badge.svg)
-============
+# vb2homebank ![Python application](https://github.com/dw0111/vb2homebank/workflows/Python%20application/badge.svg)
 
-This script converts CSV account reports from [Deutsche Kreditbank (DKB)](https://www.dkb.de) to a
+This script converts CSV account reports from german Volksbank to a
 CSV format that can be imported by the personal finance software
 [Homebank](http://homebank.free.fr/).
 
-You can find further instructions on [my blog](http://www.hamvocke.com/blog/import-dkb-accounts-into-homebank/).
+## How to run the script
 
-How to run the script
----------------------
-The script can detect whether you're converting a cash or visa account based on a simple heuristic. Simply run:
+To convert a file run:
 
-	./dkb2homebank.py yourDKBExportFile.csv
+    ./vb2homebank.py yourDKBExportFile.csv
 
-If the heuristic fails, you can pass the account type with the `-v|-c` switch:
+You can also choose an alternative path for your output file, if the standard "homebank.csv" in the working directory doesn't do it for you. Use `--output-file` or `-o` for that:
 
-To convert a _DKB Cash_ file run:
-    
-    ./dkb2homebank.py --cash yourCashReportFile.csv
+    ./vb2homebank.py yourCashReportFile.csv --output-file ~/Documents/Finances/import_to_homebank.csv
 
-To convert a _Visa_ file run:
-    
-    ./dkb2homebank.py --visa yourVisaReportFile.csv
-    
-You can also choose an alternative path for your output file, if the standard "cashHomebank.csv" or "visaHomebank.csv" in the working directory doesn't do it for you. Use `--output-file` or `-o` for that:
- 
-    ./dkb2homebank.py --cash yourCashReportFile.csv --output-file ~/Documents/Finances/import_to_homebank.csv
+## Importing into Homebank
 
-
-Importing into Homebank
------------------------
 Import the converted CSV file into Homebank by going to `File -> Import` and selecting the _output_ file you got when running your script.
 
 **Note**: If Homebank tells you that your CSV file is invalid, go to `Settings -> Import/Export` and make sure that the `Delimiter` is set to `semicolon` and try importing again.
 
-Requirements
-------------
-To run this script, you need Python 3.4 or higher. I've verified that the exported CSV can be imported successfully on Homebank *5.0.0* and above.
+## Requirements
 
-Run the tests
--------------
+To run this script, you need Python 3.4 or higher. I've verified that the exported CSV can be imported successfully on Homebank _5.0.0_ and above.
+
+## Run the tests
+
 I have included a (admittedly very small) set of tests to help a little bit during development.
 These tests use Python's _unittest_ module and can be executed using:
-    
-    ./dkb2homebankTest.py
+
+    ./vb2homebankTest.py
 
 You can also test the script manually by using the provided testfiles:
 
-    ./dkb2homebank.py --cash testfiles/cash.csv
-   
-or
-
-    ./dkb2homebank.py --visa testfiles/visa.csv
+    ./vb2homebank.py testfiles/test.csv
--- vb2homebankTest.py
+++ vb2homebankTest.py
@@ -2,69 +2,68 @@
 
 from builtins import ResourceWarning
 import unittest
-import dkb2homebank
+import vb2homebank
 import os
 import warnings
 import tempfile
 import subprocess
 
 
-class DKB2HomebankTest(unittest.TestCase):
+class Vb2HomebankTest(unittest.TestCase):
     def setUp(self):
         warnings.simplefilter("ignore", ResourceWarning)
 
     def testShouldConvertCashFile(self):
         with open(r'testfiles/cash.csv', encoding='iso-8859-1') as csv_file:
-            dkb2homebank.convert_DKB_cash(csv_file, 'cashHomebank.csv')
+            vb2homebank.convert_vb_cash(csv_file, 'cashHomebank.csv')
             csv_file.seek(0)
-            lineNumber = len(dkb2homebank.find_transaction_lines(csv_file))
+            lineNumber = len(vb2homebank.find_transaction_lines(csv_file))
             self.assertEqual(lineNumber, 2)
 
     def testShouldConvertCashFileAndWriteToAlternativeOutputDir(self):
         with open(r'testfiles/cash.csv', encoding='iso-8859-1') as csv_file:
             tmpdir = tempfile.gettempdir()
-            dkb2homebank.convert_DKB_cash(csv_file, os.path.join(tmpdir, "cashHomebank.csv"))
+            vb2homebank.convert_vb_cash(csv_file, os.path.join(tmpdir, "cashHomebank.csv"))
 
     def testThrowErrorForEmptyCashFile(self):
         with self.assertRaises(ValueError) as context:
             with open('testfiles/cash_empty.csv', encoding='iso-8859-1') as csv_file:
-                dkb2homebank.convert_DKB_cash(csv_file)
+                vb2homebank.convert_vb_cash(csv_file)
         self.assertTrue("Can't convert CSV file without header line" in str(context.exception))
 
     def testShouldConvertVisaFile(self):
         with open('testfiles/visa.csv', encoding='iso-8859-1') as csv_file:
-            dkb2homebank.convert_visa(csv_file, 'visaHomebank.csv')
+            vb2homebank.convert_visa(csv_file, 'visaHomebank.csv')
             csv_file.seek(0)
-            lineNumber = len(dkb2homebank.find_transaction_lines(csv_file))
+            lineNumber = len(vb2homebank.find_transaction_lines(csv_file))
         self.assertEqual(lineNumber, 4)
 
     def testShouldConvertVisaFileWithRange(self):
         with open('testfiles/visaRange.csv', encoding='iso-8859-1') as csv_file:
-            dkb2homebank.convert_visa(csv_file, 'visaHomebank.csv')
+            vb2homebank.convert_visa(csv_file, 'visaHomebank.csv')
             csv_file.seek(0)
-            lineNumber = len(dkb2homebank.find_transaction_lines(csv_file))
+            lineNumber = len(vb2homebank.find_transaction_lines(csv_file))
             self.assertEqual(lineNumber, 1)
 
     def tearDown(self):
-        self.delete('cashHomebank.csv')
-        self.delete('visaHomebank.csv')
+        self.delete('homebank.csv')
 
     def delete(self, filename):
         if os.path.isfile(filename):
             os.remove(filename)
 
 
-class DKB2HomebankFunctionalTest(unittest.TestCase):
+class Vb2HomebankFunctionalTest(unittest.TestCase):
     def testShouldRunScriptWithCashParameter(self):
-        result = subprocess.run(["./dkb2homebank.py", "--cash", "testfiles/cash.csv"])
+        result = subprocess.run(["./vb2homebank.py", "--cash", "testfiles/cash.csv"])
         self.assertEqual(0, result.returncode)
 
     def testShouldRunScriptWithVisaParameter(self):
-        result = subprocess.run(["./dkb2homebank.py", "--visa", "testfiles/visa.csv"])
+        result = subprocess.run(["./vb2homebank.py", "--visa", "testfiles/visa.csv"])
         self.assertEqual(0, result.returncode)
 
     def testShouldRunScriptWithOutputParameter(self):
-        result = subprocess.run(["./dkb2homebank.py", "--cash", "testfiles/cash.csv", "--output-file", "/tmp/dkb2homebank.csv"])
+        result = subprocess.run(["./vb2homebank.py", "--cash", "testfiles/cash.csv", "--output-file", "/tmp/vb2homebank.csv"])
         self.assertEqual(0, result.returncode)
 
 
